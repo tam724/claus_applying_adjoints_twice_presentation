b478684b5a10bff196811d147069ba67ab37cb18
Tamme Claus <tamme.claus@rwth-aachen.de>
2024-05-22 12:07:38 +0200

    more präsi

---
+++ figures/poisson/build_webm.sh
@@ -0,0 +1,3 @@
+# ffmpeg -r 10 -width 1200 -i optimization_measurements/%03d.svg optimization_measurements.webm
+# ffmpeg -r 10 -width 1200 -i optimization_material/%03d.svg optimization_material.webm
+ffmpeg -r 10 -width 1200 -i optimization_error/%03d.svg optimization_error.webm
